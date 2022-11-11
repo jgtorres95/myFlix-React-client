ec479244306690709bd3ff0d389a3c12a7872be0
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-10 17:15:42 -0800

    Update movies-list.jsx

--- src/components/movies-list/movies-list.jsx
+++ src/components/movies-list/movies-list.jsx
@@ -1,40 +1,47 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Col from 'react-bootstrap/Col';
-import { connect } from 'react-redux';
+import React from "react";
+import PropTypes from "prop-types";
+import Col from "react-bootstrap/Col";
+import { connect } from "react-redux";
 
-import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
-import { MovieCard } from '../movie-card/movie-card';
+import VisibilityFilterInput from "../visibility-filter-input/visibility-filter-input";
+import { MovieCard } from "../movie-card/movie-card";
 
 // extract visibilityFilter state from store
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   const { visibilityFilter } = state;
   return { visibilityFilter };
 };
 
-// create MoviesList component 
+// create MoviesList component
 function MoviesList(props) {
-
   const { movies, visibilityFilter, handleFavorite } = props;
   let filteredMovies = movies;
 
   // filter movies based on user input
-  if (visibilityFilter !== '') {
-    filteredMovies = movies.filter(m => m.Title.toLowerCase().includes(visibilityFilter.toLowerCase()));
+  if (visibilityFilter !== "") {
+    filteredMovies = movies.filter((m) =>
+      m.Title.toLowerCase().includes(visibilityFilter.toLowerCase())
+    );
   }
 
   if (!movies) return <div className="main-view" />;
 
-  return <>
-    <Col md={12} style={{ margin: '1em' }}>
-      <VisibilityFilterInput visibilityFilter={visibilityFilter} />
-    </Col>
-    {filteredMovies.map(m => (
-      <Col md={3} key={m._id}>
-        <MovieCard movie={m} handleFavorite={id => handleFavorite(id)} />
+  return (
+    <>
+      <Col md={12} style={{ margin: "1em" }}>
+        <VisibilityFilterInput visibilityFilter={visibilityFilter} />
       </Col>
-    ))}
-  </>;
+      {filteredMovies.map((m) => (
+        <Col md={4} key={m._id}>
+          <MovieCard
+            className="movie__card"
+            movie={m}
+            handleFavorite={(id) => handleFavorite(id)}
+          />
+        </Col>
+      ))}
+    </>
+  );
 }
 
 export default connect(mapStateToProps)(MoviesList);
@@ -44,6 +51,6 @@ MoviesList.propTypes = {
   movies: PropTypes.shape({
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
-    ImagePath: PropTypes.string.isRequired
+    ImagePath: PropTypes.string.isRequired,
   }).isRequired,
-}
+};
