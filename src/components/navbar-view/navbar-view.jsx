aa1495b534e32f85096724e0eb0cebf6aa4dd949
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-17 22:26:47 -0800

    incorporate loggedIn prop

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,45 +1,55 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import './navbar-view.scss';
-import { Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-
-
-
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "./navbar-view.scss";
+import { Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
 
 // create NavBarView component
 export class NavBarView extends React.Component {
-
   // sign out user by clearing local storage
   handleSignOut() {
     localStorage.clear();
-    window.open('/', '_self');
+    window.open("/", "_self");
   }
 
   render() {
+    const { loggedIn } = this.props;
+    console.log(loggedIn);
     const username = localStorage.getItem("user");
-    const home = '/';
+    const home = "/";
     const profile = `/users/${username}`;
 
-
     return (
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="danger" variant="dark" sticky="top">
         <Container>
-          <Navbar.Brand href="/">myFlix</Navbar.Brand>
-          <Nav.Item>
-            <Nav.Link as={Link} to={home}>Home</Nav.Link>
+          <Navbar.Brand href="/" className="nav__brand">
+            myFlix
+          </Navbar.Brand>
+          <Nav.Item className={loggedIn === false ? "hidden" : ""}>
+            <Nav.Link className="nav__link" as={Link} to={home}>
+              <AiOutlineHome />
+            </Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-            <Nav.Link as={Link} to={profile}>Profile</Nav.Link>
+          <Nav.Item className={loggedIn === false ? "hidden" : ""}>
+            <Nav.Link className="nav__link" as={Link} to={profile}>
+              <BsPerson />
+            </Nav.Link>
           </Nav.Item>
-          <Nav.Item>
-            <Button onClick={() => { this.handleSignOut() }}>Sign Out</Button>
+          <Nav.Item className={loggedIn === false ? "hidden" : ""}>
+            <Button
+              variant="light"
+              onClick={() => {
+                this.handleSignOut();
+              }}
+            >
+              Sign Out
+            </Button>
           </Nav.Item>
         </Container>
       </Navbar>
     );
   }
 }
-
-
