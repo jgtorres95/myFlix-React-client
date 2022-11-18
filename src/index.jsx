1f6c24a52adbd0733c74f05fb6c50d0e0a9fe414
jgtorres95 <78467454+jgtorres95@users.noreply.github.com>
2022-11-17 22:29:31 -0800

    remove Container

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Container from 'react-bootstrap/Container';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-import moviesApp from './reducers/reducers';
+import React from "react";
+import ReactDOM from "react-dom";
+import Container from "react-bootstrap/Container";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
+import moviesApp from "./reducers/reducers";
 
-import MainView from './components/main-view/main-view';
+import MainView from "./components/main-view/main-view";
 
 // bundle `./index.scss`
-import './index.scss';
+import "./index.scss";
 
 // create store
 const store = createStore(moviesApp, devToolsEnhancer());
@@ -19,16 +19,14 @@ class MyFlixApplication extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Container>
-          <MainView/>
-        </Container>
+        <MainView />
       </Provider>
     );
   }
 }
 
 //Find root of app
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 //Render app in the root DOM element
 ReactDOM.render(React.createElement(MyFlixApplication), container);
